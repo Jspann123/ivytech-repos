ee9f3aee8f0ebc46a967d944220c6763e9a2d335
Jspann123 <jspann1@ivytech.edu>
2025-04-28 18:59:55 -0400

    Add files via upload

---
+++ its raining cats and dogs.py
@@ -0,0 +1,13 @@
+# This program will print "It is raining cats and dogs."
+# Variables are defined below with proper documentation.
+# Set variable c to "cats."
+c = "cats"
+
+# Set variable d to "dogs."
+d = "dogs"
+
+# Set variable s to the full sentence, combining text and variables.
+s = "It is raining " + c + " and " + d + "."
+
+# Print the final sentence stored in variable s.
+print(s)
