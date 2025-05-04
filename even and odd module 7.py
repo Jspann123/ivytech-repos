5a1880581e46342f82117672146f511da22f8868
Jspann123 <jspann1@ivytech.edu>
2025-05-04 13:41:29 -0400

    Add files via upload

---
+++ even and odd module 7.py
@@ -0,0 +1,7 @@
+number_list = [3, 8, 15, 22, 7, 10, 5, 18, 1, 12, 9, 4, 17, 6, 13]
+
+for number in number_list:
+    if number % 2 == 0:
+        print(str(number) + " is even")
+    else:
+        print(str(number) + " is odd")
---
+++ even and odd module7.2.py
@@ -0,0 +1,19 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/jonat/AppData/Local/Programs/Python/Python313/module 72.py =
+3 is odd
+8 is even
+15 is odd
+22 is even
+7 is odd
+10 is even
+5 is odd
+18 is even
+1 is odd
+12 is even
+9 is odd
+4 is even
+17 is odd
+6 is even
+13 is odd
